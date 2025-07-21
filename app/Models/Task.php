e4027cf56086a4cfb7b778c5df8e22dee00f2356
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:49:49 -0300

    Configuring the Tasks model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Enums\TaskStatus;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'describe',
+        'slug',
+        'status',
+        'user_id',
+        'expiration_date',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class,
+            'expiration_date' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

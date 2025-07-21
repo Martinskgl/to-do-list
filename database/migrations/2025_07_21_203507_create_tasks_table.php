3e99108c9ec23d489a2485ca4d4dccd4d42d53ed
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:41:06 -0300

    Migration and Enums for use migration created

--- database/migrations/2025_07_21_203507_create_tasks_table.php
+++ database/migrations/2025_07_21_203507_create_tasks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\TaskStatus;
 
 return new class extends Migration
 {
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('describe')->nullable();
+            $table->string('slug')->unique();
+            $table->enum('status', ['pending', 'in_progress', 'completed', 'cancelled'])->default('pending');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('expiration_date')->nullable();
             $table->timestamps();
         });
     }

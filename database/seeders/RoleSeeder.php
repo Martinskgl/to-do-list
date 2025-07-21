0287f17619aef419a9d500236e95056e62356a92
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:51:56 -0300

    Role seeder has been configured

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Role::create(['slug' => 'admin', 'label' => 'admin']);
+        Role::create(['slug' => 'user', 'label' => 'user']);
     }
 }

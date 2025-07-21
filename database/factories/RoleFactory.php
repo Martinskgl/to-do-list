a7a1e5d37d0fbfb916676c85a12d5a02537c2caf
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:44:16 -0300

    add factory for Role

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -4,20 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
- */
 class RoleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $slug = $this->faker->randomElement(['user', 'admin']);
         return [
-            //
+            'slug' => $slug,
+            'label' => $slug,
         ];
     }
 }

63e8f0596f7a9c084da78aa4cd25dd3fe65a7b14
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:45:15 -0300

    add factory for Task

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,21 +3,21 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Enums\TaskStatus;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
- */
 class TaskFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $title = $this->faker->sentence(3);
         return [
-            //
+            'title' => $title,
+            'describe' => $this->faker->paragraph(),
+            'slug' => str($title)->slug(),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed', 'cancelled']),
+            'user_id' => User::factory(),
+            'expiration_date' => $this->faker->optional()->dateTimeBetween('now', '+1 month'),
         ];
     }
 }

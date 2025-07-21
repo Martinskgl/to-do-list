4a5db3bd4e0890965294822b16564a4a58597161
João Martins <joaomartins72021@gmail.com>
2025-07-21 17:46:47 -0300

    configured model for role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    /** @use HasFactory<\Database\Factories\RoleFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'label',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
